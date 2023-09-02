618baaa2b8c251d13070927a84eebf14e7a3ca4c
Yelyzaveta Blyzynska <y.blyzynska@gmail.com>
2023-09-02 21:04:30 +0300

    improved unit conversion and current date

--- script.js
+++ script.js
@@ -32,8 +32,12 @@ let celciusLink = document.querySelector("#to-celcius");
 let fahrenheitLink = document.querySelector("#to-fahrenheit");
 
 form.addEventListener("submit", onSearch);
-celciusLink.addEventListener("click", showCelcius);
-fahrenheitLink.addEventListener("click", showFahrenheit);
+celciusLink.addEventListener("click", () => {
+  showUnits("metric");
+});
+fahrenheitLink.addEventListener("click", () => {
+  showUnits("imperial");
+});
 currentButton.addEventListener("click", getCurrentData);
 
 function onSearch(event) {
@@ -79,7 +83,7 @@ function logData(response) {
     `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
   );
 
-  let wUrl = `http://api.timezonedb.com/v2.1/get-time-zone?key=S4J8L7B44L2Z&format=json&by=position&lat=${response.data.coordinates.latitude}&lng=${response.data.coordinates.longitude}`;
+  let wUrl = `https://api.timezonedb.com/v2.1/get-time-zone?key=S4J8L7B44L2Z&format=json&by=position&lat=${response.data.coordinates.latitude}&lng=${response.data.coordinates.longitude}`;
   axios.get(wUrl).then(logTime);
 
   let fUrl = `https://api.shecodes.io/weather/v1/forecast?lat=${response.data.coordinates.latitude}&lon=${response.data.coordinates.longitude}&key=${API_KEY}&units=metric`;
@@ -100,11 +104,24 @@ function logForecast(response) {
 function logTime(response) {
   let currentTime = document.querySelector(".time");
   let date = response.data.formatted;
+  let dateDay = new Date(response.data.timestamp * 1000 - 360000);
   let hours = date.slice(11, 13);
   let minutes = date.slice(14, 16);
+  let selectedDay = document.querySelector("#today-day");
+  let selectedDate = document.querySelector("#today-date");
+
+  let day = DAYS[dateDay.getDay()];
+  let month = MONTHS[dateDay.getMonth()];
+  let dateN = dateDay.getDate();
+  let year = dateDay.getFullYear();
+
+  let dayString = `${day}, `;
+  let dateString = `${month} ${dateN}, ${year}`;
 
   let time = `${hours}:${minutes}`;
 
+  changeElementInnerHtml(selectedDay, dayString);
+  changeElementInnerHtml(selectedDate, dateString);
   changeElementInnerHtml(currentTime, time);
 }
 
@@ -112,43 +129,69 @@ function changeElementInnerHtml(el, content) {
   el.innerHTML = content;
 }
 
-function showCelcius() {
+function showUnits(units) {
   let currentTemperature = document.getElementById("current-temp");
-  let content = currentTemperature.innerHTML;
-  let unit = content.charAt(content.length - 1);
-  let value = content.slice(0, -2);
-  if (unit !== "C") {
-    content = `${convertToCelcius(value).toFixed(0)}°C`;
+  let windUnit = document.getElementById("wind-unit");
+  let wind = document.getElementById("wind");
+  let minTemp = document.getElementById("min-temp");
+  let maxTemp = document.getElementById("max-temp");
+  let minUnit = document.getElementById("min-unit");
+  let maxUnit = document.getElementById("max-unit");
+  let currentContent = currentTemperature.innerHTML;
+
+  let unit = currentContent.charAt(currentContent.length - 1);
+  let value = currentContent.slice(0, -2);
+  if (unit !== "C" && units === "metric") {
+    changeElementInnerHtml(
+      currentTemperature,
+      `${convertToCelcius(value).toFixed(0)}°C`
+    );
+    changeElementInnerHtml(
+      minTemp,
+      `${convertToCelcius(minTemp.innerHTML).toFixed(0)}`
+    );
+    changeElementInnerHtml(
+      maxTemp,
+      `${convertToCelcius(maxTemp.innerHTML).toFixed(0)}`
+    );
+    changeElementInnerHtml(minUnit, `°C`);
+    changeElementInnerHtml(maxUnit, `°C`);
+    changeElementInnerHtml(windUnit, `km/h`);
+    changeElementInnerHtml(wind, convertToKmh(wind.innerHTML).toFixed(2));
+  } else if (unit !== "F" && units === "imperial") {
+    changeElementInnerHtml(
+      currentTemperature,
+      `${convertToFahrenheit(value).toFixed(0)}°F`
+    );
+    changeElementInnerHtml(
+      minTemp,
+      `${convertToFahrenheit(minTemp.innerHTML).toFixed(0)}`
+    );
+    changeElementInnerHtml(
+      maxTemp,
+      `${convertToFahrenheit(maxTemp.innerHTML).toFixed(0)}`
+    );
+    changeElementInnerHtml(minUnit, `°F`);
+    changeElementInnerHtml(maxUnit, `°F`);
+    changeElementInnerHtml(windUnit, `mph`);
+    changeElementInnerHtml(wind, convertToMph(wind.innerHTML).toFixed(2));
   }
-  changeElementInnerHtml(currentTemperature, content);
-  currentTemperature.innerHTML = content;
 }
 
-function showFahrenheit() {
-  let currentTemperature = document.getElementById("current-temp");
-  let content = currentTemperature.innerHTML;
-  let unit = content.charAt(content.length - 1);
-  let value = content.slice(0, -2);
-  if (unit !== "F") {
-    content = `${convertToFahrenheit(value).toFixed(0)}°F`;
-  }
-  changeElementInnerHtml(currentTemperature, content);
-}
+// function changeDateData(target_date) {
+//   let selectedDay = document.querySelector("#today-day");
+//   let selectedDate = document.querySelector("#today-date");
+//   let day = DAYS[target_date.getDay()];
+//   let month = MONTHS[target_date.getMonth()];
+//   let date = target_date.getDate();
+//   let year = target_date.getFullYear();
 
-function changeDateData(target_date) {
-  let selectedDay = document.querySelector("#today-day");
-  let selectedDate = document.querySelector("#today-date");
-  let day = DAYS[target_date.getDay()];
-  let month = MONTHS[target_date.getMonth()];
-  let date = target_date.getDate();
-  let year = target_date.getFullYear();
+//   let dayString = `${day}, `;
+//   let dateString = `${month} ${date}, ${year}`;
 
-  let dayString = `${day}, `;
-  let dateString = `${month} ${date}, ${year}`;
-
-  changeElementInnerHtml(selectedDay, dayString);
-  changeElementInnerHtml(selectedDate, dateString);
-}
+//   changeElementInnerHtml(selectedDay, dayString);
+//   changeElementInnerHtml(selectedDate, dateString);
+// }
 
 //format text, convert to Fahrenheit, showAlert functions
 function format(city) {
@@ -167,6 +210,14 @@ function format(city) {
   return city;
 }
 
+function convertToMph(kmh) {
+  return kmh / 1.609344;
+}
+
+function convertToKmh(mph) {
+  return mph * 1.609344;
+}
+
 //(9/5)*t°C+32
 function convertToFahrenheit(celcius) {
   return (9 / 5) * celcius + 32;
@@ -176,6 +227,6 @@ function convertToCelcius(fahr) {
   return (fahr - 32) / (9 / 5);
 }
 
-changeDateData(now);
+// changeDateData(now);
 // changeTime();
 getCurrentData();
